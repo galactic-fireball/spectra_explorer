e22297aefba43faa8dadaf543251b867846c5e33
galactic-fireball <sdoan1@gmail.com>
2025-06-21 15:53:10 -0400

    style upgrade; added config panel; added filters

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ save_test_file = target_file.parent.joinpath(target_file.stem+'_test.csv')
 spec_dir = data_dir.joinpath('spec_data')
 json_cont = {'ContentType':'application/json'}
 
+default_target_cols = ['SDSS_NAME','P-M-F','SpecObjID','RA','DEC','Z','lam_min','lam_max']
+
 app = Flask(__name__)
 
 # TODO: better way to do this?
@@ -40,15 +42,96 @@ def get_features():
 	return jsonify(CACHE['features']), 200, json_cont
 
 
-@app.route('/api/features')
+@app.route('/api/metrics')
 def get_metrics():
 	return jsonify(CACHE['metrics']), 200, json_cont
 
 
-# TODO: add filters
 @app.route('/api/specs')
 def get_specs():
-	return jsonify(list(CACHE['tdf']['P-M-F'].values)), 200, json_cont
+	print('get_specs')
+	print(request)
+	print(request.args)
+	print(len(request.args))
+
+	if (len(request.args) == 0) or not (request.args.get('feature', None)):
+		return jsonify({'specs':list(CACHE['tdf']['P-M-F'].values)}), 200, json_cont
+
+	tdf = CACHE['tdf']
+	feature = request.args.get('feature')
+	feat_cols = [c for c in tdf.columns.to_list() if feature in c]
+	rdf = tdf[default_target_cols+feat_cols]
+	insp_attr = feature+'_INSPECT'
+
+	def filter_insp_status(df, filt, val):
+		if val: return df
+		if insp_attr not in df: return df
+		print('Filtering %s'%filt)
+
+		if filt == 'inspected':
+			return df[np.nan(rdf[insp_attr])]
+		if filt == 'not_inspected':
+			return df[~np.nan(rdf[insp_attr])]
+		if filt == 'detections':
+			return df[rdf[insp_attr] != 1.0]
+		if filt == 'non_detections':
+			return df[rdf[insp_attr] != -1.0]
+		elif filt == 'unclear':
+			return df[rdf[insp_attr] != 0.0]
+
+	for filt, val in request.args.items():
+		cprev = len(rdf)
+		if filt == 'feature': continue
+
+		print(filt)
+		if filt in ['inspected', 'not_inspected', 'detections', 'non_detections', 'unclear']:
+			rdf = filter_insp_status(rdf, filt, val)
+			print('rdf: %d -> %d'%(cprev,len(rdf)))
+			continue
+
+		if '_' not in filt:
+			print('invalid filter name: %s'%filt)
+			continue
+
+		metric, ident = filt.rsplit('_',1)
+		if not metric in CACHE['metrics']:
+			print('invalid metric name: %s'%metric)
+			continue
+
+		try:
+			fval = float(val)
+		except:
+			print('invalid value: %s'%val)
+			continue
+
+		attr = feature+'_'+metric
+		if ident == 'min':
+			print('Filtering: %s >= %f'%(attr,fval))
+			rdf = rdf[rdf[attr] >= fval]
+		elif ident == 'max':
+			print('Filtering: %s <= %f'%(attr,fval))
+			rdf = rdf[rdf[attr] <= fval]
+
+		print('rdf: %d -> %d'%(cprev,len(rdf)))
+
+	specs = list(rdf['P-M-F'].values)
+	print('returning %d specs'%len(specs))
+
+	if insp_attr in rdf:
+		insp = len(rdf[~np.isnan(rdf[insp_attr])])
+		detects = len(rdf[rdf[insp_attr] == 1.0])
+	else:
+		insp = 0
+		detects = 0
+
+	stats = {
+		'total': len(specs),
+		'inspected': insp,
+		'detections': detects
+	}
+
+	# print(stats)
+	return jsonify({'specs':specs,'stats':stats}), 200, json_cont
 
 
 @app.route('/api/spec')
@@ -98,7 +181,7 @@ def mark_inspect():
 
 @app.route('/api/save')
 def save():
-	CACHE['tdf'].to_csv(save_test_file, index=False)
+	CACHE['tdf'].to_csv(target_file, index=False)
 	return jsonify({'success':True}), 200, json_cont
 
 
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,8 +1,21 @@
 
 const colors = {
-    'grey': '#8b9099',
-    'orange': '#ca6702'
+    'grey': '#3c4043',
+    'dark-bg': '#222222',
+    'pale-green': '#638687',
+    'dark-red': '#721605',
+    'red': '#ae1c0c',
+    'orange': '#de7b13',
+    'yellow': '#fcd55b',
 };
+
+const keyCodes = {
+    'space': 32,
+    'left': 37,
+    'up': 38,
+    'right': 39,
+    'down': 40,
+}
 
 // const FHILines = ['FeXI_7892', 'SXII_7609', 'FeX_6374', 'FeVII_6087', 'FeVII_5720', 'ArX_5533', 'FeVI_5335', 'CaV_5309', 'FeXIV_5303', 'FeVII_5276', 'FeVI_5176', 'FeVII_5159', 'FeVI_5146', 'FeVII_4893', 'FeV_4181', 'FeV_3891', 'FeV_3839', 'FeVII_3759', 'NeV_3426', 'NeV_3346', 'NeIV_2424'];
 const FHILines = ['FeVII_6087', 'NeV_3426'];
@@ -24,6 +37,24 @@ const saveURL = '/api/save';
 
 const viewerID = 'spectra-viewer';
 
+var statDict = {
+    total: 0,
+    inspected: 0,
+    uninspected: 0,
+    detections: 0,
+}
+
+
+// TODO: plot params dict
+var modelOn = true;
+var userWaveMin = 0;
+var userWaveMax = 999999;
+
+
+function print(msg) {
+    console.log(msg);
+}
+
 
 function initPlotViewer() {
     Plotly.newPlot(viewerID, [{x:[],y:[]}]);
@@ -32,35 +63,44 @@ function initPlotViewer() {
 
 function generatePlot() {
 
-
     var data_trace = {
         x: targetSpecInfo.wave,
         y: targetSpecInfo.data,
         type: 'scatter',
         mode: 'lines',
+        // marker: {color:'white'},
         name: 'Data',
     };
 
-    var model0_trace = {
-        x: targetSpecInfo.wave,
-        y: targetSpecInfo.ncomp0,
-        type: 'scatter',
-        mode: 'lines',
-        name: 'ncomp=0',
-    };
+    var traces = [data_trace];
 
-    var model1_trace = {
-        x: targetSpecInfo.wave,
-        y: targetSpecInfo.ncomp1,
-        type: 'scatter',
-        mode: 'lines',
-        name: 'ncomp=1',
-    };
+    if (modelOn) {
 
-    var traces = [data_trace,model0_trace,model1_trace];
+        var model0_trace = {
+            x: targetSpecInfo.wave,
+            y: targetSpecInfo.ncomp0,
+            type: 'scatter',
+            mode: 'lines',
+            // marker: {color:'yellow'},
+            name: 'ncomp=0',
+        };
 
-    const wave_min = Math.min(...targetSpecInfo.wave);
-    const wave_max = Math.max(...targetSpecInfo.wave);
+        var model1_trace = {
+            x: targetSpecInfo.wave,
+            y: targetSpecInfo.ncomp1,
+            type: 'scatter',
+            mode: 'lines',
+            // marker: {color:'blue'},
+            name: 'ncomp=1',
+        };
+
+        traces.push(model0_trace);
+        traces.push(model1_trace);
+    }
+
+    const wave_min = Math.max(Math.min(...targetSpecInfo.wave), userWaveMin);
+    const wave_max = Math.min(Math.max(...targetSpecInfo.wave), userWaveMax);
+    console.log(wave_min+'-'+wave_max);
     const spec_min = Math.min(...targetSpecInfo.data);
     const spec_max = Math.max(...targetSpecInfo.data);
     const y0 = spec_min-(spec_max*0.1);
@@ -71,7 +111,7 @@ function generatePlot() {
         if (center < wave_min || center > wave_max) { continue; }
         line_color = 'grey';
         if (name.replace('NA_','') === focusFeature) {
-            line_color = 'orange';
+            line_color = 'red';
         }
         vlines.push({
             type:'line', x0:center, x1:center, y0:y0, y1:y1,
@@ -81,9 +121,13 @@ function generatePlot() {
 
     var layout = {
         title: {text: targetSpecInfo.name},
-        xaxis: {title: {text:'λ rest (Å)'}},
-        yaxis: {title: {text:'f_λ (10^-17 erg s^-1 cm^-2 Å^-1)'}},
+        // paper_bgcolor: colors['dark-bg'],
+        // plot_bgcolor: colors['dark-bg'],
+        // font: {color: colors['pale-green'], size:16},
+        xaxis: {range: [wave_min,wave_max], title: {text:'λ rest (Å)'}}, // color:colors['pale-green']}},
+        yaxis: {title: {text:'f_λ (10^-17 erg s^-1 cm^-2 Å^-1)'}}, // color:colors['pale-green']}},
         shapes: vlines,
+        legend: {orientation:'h'}
     };
 
     var config = {
@@ -101,11 +145,21 @@ async function setTargetInfo(specName) {
     };
 
     try {
-        const response = await fetch(specURL + new URLSearchParams(get_data).toString());
+        let response = await fetch(specURL + new URLSearchParams(get_data).toString());
         targetSpecInfo = await response.json();
         targetSpecInfo['name'] = specName;
     } catch(error) {
         console.log('Fetch error: ' + error);
+    }
+}
+
+
+async function getMetricData() {
+    try {
+        let response = await fetch(metricsURL);
+        metrics = await response.json();
+    } catch(error) {
+        console.log('Fetch error: ' + error)
     }
 }
 
@@ -123,7 +177,7 @@ async function getFeatureData() {
 async function getSpecNames() {
     try {
         const response = await fetch(specsURL);
-        specNames = await response.json();
+        specNames = await response.json()['specs'];
     } catch(error) {
         console.log('Fetch error: ' + error);
     }
@@ -149,6 +203,11 @@ function markInspect(val) {
     }
 }
 
+function prevSpec() {
+    curSpecIdx--;
+    plotSpec();
+}
+
 
 function nextSpec() {
     curSpecIdx++;
@@ -166,28 +225,180 @@ function saveInspect() {
 
 
 function initInspectButtons() {
-    const no = document.getElementById('insp-no-button');
-    no.onclick = function () {
+    no_click = function () {
+        statDict.inspected++;
+        genStatTable();
         markInspect(-1);
         nextSpec();
     };
+    const no = document.getElementById('insp-no-button');
+    no.onclick = no_click;
 
-    const unclear = document.getElementById('insp-unclear-button');
-    unclear.onclick = function () {
+    unclear_click = function () {
+        statDict.inspected++;
+        genStatTable();
         markInspect(0);
         nextSpec();
     };
+    const unclear = document.getElementById('insp-unclear-button');
+    unclear.onclick = unclear_click
 
-    const yes = document.getElementById('insp-yes-button');
-    yes.onclick = function () {
+    yes_click = function () {
+        statDict.inspected++;
+        statDict.detections++;
+        genStatTable();
         markInspect(1);
         nextSpec();
     };
+    const yes = document.getElementById('insp-yes-button');
+    yes.onclick = yes_click
 
-    const save = document.getElementById('save-button');
-    save.onclick = function () {
+    save_click = function () {
         saveInspect();
     };
+    const save = document.getElementById('save-button');
+    save.onclick = save_click
+
+    model_click = function () {
+        modelOn = !modelOn;
+        generatePlot();
+    };
+    const model = document.getElementById('model-button');
+    model.onclick = model_click
+
+    document.body.onkeydown = function(e) {
+        if (e.key === 'y' || e.key === ' ' || e.keyCode == 32) {
+            yes_click();
+        } else if (e.key === 'n' || e.key === 'q') {
+            no_click();
+        } else if (e.key === 'u' || e.key === 'r') {
+            unclear_click();
+        } else if (e.key === 's') {
+            save_click();
+        } else if (e.key === 'm') {
+            model_click();
+        } else if (e.key === 'ArrowLeft') {
+            if (curSpecIdx === 0) { return; }
+            prevSpec();
+        } else if (e.key === 'ArrowRight') {
+            nextSpec();
+        }
+    };
+}
+
+
+function initMetricFilters() {
+    const labelContainer = document.getElementById('metric-label-container');
+    const minContainer = document.getElementById('metric-min-container');
+    const maxContainer = document.getElementById('metric-max-container');
+
+    for (const m of metrics) {
+        let l = document.createElement('label')
+        l.innerHTML = m
+        labelContainer.appendChild(l);
+
+        let mmin = document.createElement('input');
+        mmin.id = m+'_min';
+        mmin.classList.add('text-input');
+        minContainer.appendChild(mmin);
+
+        let mmax = document.createElement('input');
+        mmax.id = m+'_max';
+        mmax.classList.add('text-input');
+        maxContainer.appendChild(mmax);
+    }
+}
+
+
+function initWaveRanges() {
+    adjustWaveRange = function() {
+        min = document.getElementById('wave-min').value;
+        userWaveMin = parseFloat(min || userWaveMin);
+        max = document.getElementById('wave-max').value;
+        userWaveMax = parseFloat(max || userWaveMax);
+        console.log(userWaveMin+'-'+userWaveMax);
+        generatePlot();
+    }
+
+    const waveMin = document.getElementById('wave-min');
+    waveMin.addEventListener('focusout', adjustWaveRange);
+    waveMin.onkeydown = function(e) {
+        if (e.key === 'Enter') { adjustWaveRange(); }
+    }
+
+    const waveMax = document.getElementById('wave-max');
+    waveMax.addEventListener('focusout', adjustWaveRange);
+    waveMax.onkeydown = function(e) {
+        if (e.key === 'Enter') { adjustWaveRange(); }
+    }
+}
+
+
+function initFilter() {
+
+    async function filterClick() {
+        try {
+
+            get_data = {
+                'feature': focusFeature,
+            };
+
+            // Gather filter data
+            for (const m of metrics) {
+                let mmin = document.getElementById(m+'_min').value;
+                console.log('['+mmin.value+']');
+                if (mmin) { get_data[m+'_min'] = mmin; }
+                let mmax = document.getElementById(m+'_max').value;
+                if (mmax) { get_data[m+'_min'] = mmax; }
+            }
+
+            get_data['inspected'] = document.getElementById('insp-check').checked;
+            get_data['not_inspected'] = document.getElementById('not-insp-check').checked;
+            get_data['detections'] = document.getElementById('detect-check').checked;
+            get_data['non_detections'] = document.getElementById('non-detect-check').checked;
+            get_data['unclear'] = document.getElementById('unclear-check').checked;
+
+            // Get specs that pass filters
+            let response = await fetch(specsURL+'?' + new URLSearchParams(get_data).toString());
+            data = await response.json();
+            specNames = data['specs'];
+            stats = data['stats'];
+            console.log(stats);
+
+            // Regen as needed
+            for (const [key, val] of Object.entries(stats)) {
+                statDict[key] = val;
+            }
+
+            console.log(statDict);
+            genStatTable();
+            await plotSpec();
+
+        } catch(error) {
+            console.log('Fetch error: ' + error);
+        }
+    }
+
+    filterButton = document.getElementById('filter-button');
+    filterButton.onclick = filterClick;
+}
+
+
+function genStatTable() {
+    console.log(statDict);
+    statDict.uninspected = statDict.total - statDict.inspected;
+    document.getElementById('stat-total').textContent = statDict.total;
+    document.getElementById('stat-inspected').textContent = statDict.inspected;
+    document.getElementById('stat-uninspected').textContent = statDict.uninspected;
+    document.getElementById('stat-detects').textContent = statDict.detections;
+}
+
+
+function initConfigPanel() {
+    initMetricFilters();
+    initFilter();
+    genStatTable();
+    initWaveRanges();
 }
 
 
@@ -201,10 +412,12 @@ async function plotSpec() {
 async function start() {
     console.log('start');
     initPlotViewer();
+    await getMetricData();
+    initConfigPanel();
     initInspectButtons();
     await getFeatureData();
-    await getSpecNames();
-    await plotSpec();
+    // await getSpecNames();
+    // await plotSpec();
 }
 
 
